b720192164e21d7fc3fe8710ad2029b3a5ecb191
Сергей Догадин <me@sdogadin.ru>
2023-02-16 08:25:15 +0300

    Third review

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
